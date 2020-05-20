3d5e5313f2696ba9573e67c5e817c447ff61a41f
Mulletzzz <BrandonHartman@users.noreply.github.com>
2020-05-20 12:43:42 -0400

    Add project files.

---
+++ tower/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using tower;
+
+namespace TreehouseDefense
+{
+    class Program
+    {
+        public static void Main()
+        {
+    
+            Map map = new Map(8, 5);
+
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
