93f69c403d6e970169d5bd3a6172f952159a051b
Mulletzzz <BrandonHartman@users.noreply.github.com>
2020-05-20 15:55:37 -0400

    Created a Path Class.

--- tower/Path.cs
+++ tower/Path.cs
@@ -6,6 +6,15 @@ namespace tower
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
 
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
